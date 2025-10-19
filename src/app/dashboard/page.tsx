d74302812df52d79179bdd9a710af81bbfe4e27a
VRPunk <99029244+VRPunk9036@users.noreply.github.com>
2025-10-19 16:04:11 +0000

    compare film tokens link

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Metadata } from "next";
 
+import Link from "next/link";
+
 //Film Token Details | Guide2Film3
 export const metadata: Metadata = {
   title: 'Dashboard | Guide2Film3',
@@ -13,7 +15,9 @@ export default function DashboardPage(): React.ReactElement {
             <main className="container border-r-8 mx-auto mt-12 rounded-sm">
                 <div className="grid grid-flow-col gap-2 border-8 rounded-sm">
                     <div className="border-2 border-r-2">01</div>
-                    <div className="">02</div>
+                    <div className="">
+                        <Link href="/tokens">Compare Film Tokens</Link>
+                    </div>
                 </div>
             </main>
         </>
