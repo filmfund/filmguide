da1600c6855726b5fb3c7b5ea9bc2153d64ea8db
VRPunk <99029244+VRPunk9036@users.noreply.github.com>
2025-10-19 15:33:20 +0000

    placeholder log

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <div className="header-container grid grid-flow-col justify-between items-center p-4 border-b-2">
             <div className="">
-                <div className="logo">Guide2Film3</div>
+                <div className="logo"><img src="/next.svg" className="h-6 dark:hidden" alt="Tailwind Play" /></div>
                 <div className="slogan">Your goto Web3 film app</div>
             </div>
             <div className="header-right-section ">
