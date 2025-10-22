a91da80e9136b3066029f20bc88b30e9501bc48e
VRPunk <99029244+VRPunk9036@users.noreply.github.com>
2025-10-22 09:56:36 +0000

    fix annoying bug with suspend image being called on server render

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,10 @@ import WalletConnect from "../WalletConnect/index";
 import Image from "next/image";
 import { SuspenseImage } from "suspense-next-image";
 
+function isOnClient(): boolean {
+    return typeof window !== 'undefined';
+}
+
 export default function Header() {
     const logoAltText = "Guide2Film3 Logo";
     const logoWidth = 65;
@@ -20,20 +24,27 @@ export default function Header() {
         />
     );
 
+    let logoImageElement = fallbackImage;
+    if (isOnClient()) {
+        logoImageElement = (
+            <Suspense fallback={fallbackImage}>
+                <SuspenseImage 
+                    src="/guide2film3.gif"
+                    alt={logoAltText}
+                    width={logoWidth}
+                    height={logoHeight}
+                    unoptimized={true}
+                    className="object-contain"
+                    />
+            </Suspense>
+        );
+    }
+
     return (
         <header className="border-b border-[#A60E0E] bg-[#2b2b31]">
             <div className="container mx-auto px-6 py-4 flex items-center justify-between">
                 <div className="flex items-center gap-4">
-                    <Suspense fallback={fallbackImage}>
-                        <SuspenseImage 
-                            src="/guide2film3.gif"
-                            alt={logoAltText}
-                            width={logoWidth}
-                            height={logoHeight}
-                            unoptimized={true}
-                            className="object-contain"
-                            />
-                    </Suspense>
+                    {logoImageElement}
                     <div>
                         <h1 className="text-2xl font-bold text-[#E1C586]">Guide2Film3</h1>
                         <p className="font-semibold text-sm text-[#999999]">aka Guide to Blockchain Cinema by <a href="http://filmfund.113kw.net/" className="text-[#E1C586]">FilmFund.ETH</a> . AI-powered Web3 guide to blockchain films, funding, and decentralized cinema.</p>
